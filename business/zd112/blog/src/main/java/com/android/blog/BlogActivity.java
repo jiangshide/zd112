74f5ab8829293c9cfdf420ed9f2bf5c3c63dd07f
jiangshide <18311271399@163.com>
2019-10-07 13:34:47 +0800

    add blog call

--- business/zd112/blog/src/main/java/com/android/blog/BlogActivity.java
+++ business/zd112/blog/src/main/java/com/android/blog/BlogActivity.java
@@ -1,11 +1,19 @@
 package com.android.blog;
 
+import android.os.Bundle;
+import androidx.annotation.Nullable;
 import com.android.base.BaseActivity;
+import com.android.zdannotations.BindPath;
 
 /**
  * created by jiangshide on 2019-09-08.
  * email:18311271399@163.com
  */
+@BindPath("blog/blog")
 public class BlogActivity extends BaseActivity {
 
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    push(new BlogFragment());
+  }
 }
